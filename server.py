357072867ae6a72b55fe093c8a301068a00cee1b
Saumit <saumit2102@gmail.com>
2021-02-20 14:23:03 +0530

    version 1

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template,request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def index(page_name):
+    return render_template(page_name)
+
+
+
+def write_to_file(data):
+    with open("database.txt", "a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n{email}, {subject}, {message}")
+
+
+def write_to_csv(data):
+    with open("database.csv",mode = "a",newline='') as database2:
+        fieldnames = ['email', 'subject', 'message']
+        writer = csv.writer(database2,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        
+       
+        writer.writerow([email,subject,message])
+        
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_csv(data)
+    return redirect("/thankyou.html")
+   
+
+''' 
+scripts/activate.ps1
+$env:FLASK_APP = "server.py"
+$env:FLASK_ENV = "development"
+flask run '''   
+
